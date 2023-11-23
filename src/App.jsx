4a795ab94817bed32a4be8c3faa18c6e05b60827
Filip Rudek <wojciechrudek@gmail.com>
2023-11-23 18:48:09 +0100

    configured prettier for tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import "./App.css";
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <p className="text-3xl font-bold underline text-red-500">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    return (
+        <>
+            <p className='text-3xl font-bold text-red-500 underline'>
+                Click on the Vite and React logos to learn more
+            </p>
+        </>
+    );
 }
 
 export default App;
