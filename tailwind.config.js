25a7a30d7d210d23dc3bfc206cfb25c3c1450efe
Filip Rudek <wojciechrudek@gmail.com>
2023-12-14 23:19:02 +0100

    added custom colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,19 @@ export default {
     },
     plugins: [require('daisyui')],
     daisyui: {
-        themes: ['light'], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+        themes: [
+            {
+                izaTheme: {
+                    pureWhite: '#FFFFFF',
+                    gray: '#949494',
+                    black: '#000000',
+                    primary: '#01344C',
+                    error: '#D62620',
+                    warning: '#F5A700',
+                    success: '#2E9F40',
+                },
+            },
+        ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
         darkTheme: 'dark', // name of one of the included themes for dark mode
         base: true, // applies background color and foreground color for root element by default
         styled: true, // include daisyUI colors and design decisions for all components
