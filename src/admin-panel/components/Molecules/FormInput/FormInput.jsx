838bd1b5160f8c2bb93b12c5ed706580f1962641
Filip Rudek <wojciechrudek@gmail.com>
2024-01-05 11:53:22 +0100

    created FormInput molecule

---
+++ src/admin-panel/components/Molecules/FormInput/FormInput.jsx
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+
+const FormInput = ({ label, name, type, placeholder, register, required, error, ...rest }) => {
+    return (
+        <label className='form-control w-full'>
+            <div className={`label ${error ? 'text-crimson' : ''}`}>
+                <span className='label-text'>{error ? error.message : label}</span>
+            </div>
+            <input
+                name={name}
+                type={type}
+                placeholder={placeholder}
+                className='input input-bordered input-primary w-full'
+                {...register(name, { required: required })}
+                {...rest}
+            />
+        </label>
+    );
+};
+
+FormInput.propTypes = {
+    label: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    type: PropTypes.string.isRequired,
+    placeholder: PropTypes.string.isRequired,
+    register: PropTypes.func.isRequired,
+    onChange: PropTypes.func,
+    required: PropTypes.bool,
+    error: PropTypes.object,
+};
+
+export default FormInput;
