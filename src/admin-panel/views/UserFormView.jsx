18afa2ea21d03ddb56f490654c2a80a0fe1349c8
Filip Rudek <wojciechrudek@gmail.com>
2024-02-23 23:52:03 +0100

    fixing formCopy undefined Error

--- src/admin-panel/views/UserFormView.jsx
+++ src/admin-panel/views/UserFormView.jsx
@@ -1,19 +1,29 @@
+import { useEffect, useState, useMemo } from 'react';
 import { useForm, FormProvider } from 'react-hook-form';
 import { useAppView, useValidation } from '../../utils/hooks';
 import { joiResolver } from '@hookform/resolvers/joi';
-import { renderFormCopy } from '../../utils/helpers';
+import { isObjectEmpty, renderFormCopy } from '../../utils/helpers';
 import Form from '../components/Organism/Forms/Form';
 import StepsIndicator from '../components/Molecules/StepsIndicator/StepsIndicator';
 import CopySection from '../components/Molecules/CopySection/CopySection';
+import Loader from '../components/Atoms/Loader/Loader';
 
 const UserFormView = () => {
+    const [isLoading, setIsLoading] = useState(true);
     const [appView] = useAppView();
     const [validationSchema] = useValidation(appView);
     const methods = useForm({ mode: 'onChange', resolver: joiResolver(validationSchema) });
-    const copy = renderFormCopy(appView) || {};
+    const copy = useMemo(() => renderFormCopy(appView), [appView]);
+
+    useEffect(() => {
+        if (!isObjectEmpty(copy)) {
+            setIsLoading(false);
+        }
+    }, [copy]);
 
     return (
         <>
+            {isLoading && <Loader className='absolute left-1/2 top-1/2' />}
             <div className='grid h-full w-full grid-cols-2 grid-rows-[140px_1fr] gap-x-0 lg:gap-x-8'>
                 {appView !== 'editUser' && appView !== 'editUserCredentials' && (
                     <StepsIndicator appView={appView} />
